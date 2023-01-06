15f91a553df9b582abf65ec0e2919bd897dc0b43
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2023-01-06 18:53:20 +0100

    procédure de test de réservation

--- automatisation/reservationPossible.sql
+++ automatisation/reservationPossible.sql
@@ -1,18 +1,18 @@
 CREATE PROCEDURE 'reservationPossible'(IN IDcircuit INT, IN IDperiode INT, OUT test TINYINT)
 BEGIN
 
-    SELECT False INTO test;
+    SELECT FALSE INTO test; /* Présomption d'impossibilité de réserver */
 
-    IF (SELECT COUNT(circuitImminent.id_circuit)
+    IF (SELECT COUNT(circuitImminent.id_circuit) /* Vérification si le circuit n'est pas dans la vue des circuit dont le départ est imminent (- de 15 jours et dans le futur) */
         FROM circuitImminent
         WHERE circuitImminent.id_circuit = IDcircuit
     ) = 0 AND
-    (SELECT DATE_SUB(periode.date_depart, NOW())
-    FROM circuit, periode
+    (SELECT DATE_SUB(periode.date_depart, NOW()) /* Vérification si le la période indiquée a une date de départ postérieur à aujourd'hui */ 
+    FROM periode
     WHERE periode.id_periode = IDperiode
     ) > 0
     THEN
-
+        SELECT TRUE INTO test;
     END IF;
     
 END
