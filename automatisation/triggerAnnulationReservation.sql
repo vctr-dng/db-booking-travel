7584a6ca153b3ae5f405d1415d01d348f934b85e
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2023-01-06 15:55:26 +0100

    ajout du code a tester pour le trigger d'annulation de réservation

--- automatisation/triggerAnnulationReservation.sql
+++ automatisation/triggerAnnulationReservation.sql
@@ -3,9 +3,17 @@ AFTER UPDATE
 ON reservation FOR EACH ROW
 BEGIN
     IF !(OLD.effective <=> NEW.effective) AND NEW.effective = false THEN
+
         UPDATE circuit
         SET nb_places_reservees = nb_places_reservees - 1
         WHERE
-        circuit.id_circuit = NEW.id_circuit AND
+        circuit.id_circuit IN
+            (SELECT circuitEchue.id_circuit
+            FROM circuitEchue, devis, reservation
+            WHERE
+            circuitEchue.id_circuit = devis.id_circuit AND
+            devis.id_devis = reservation.id_devis AND
+            resevation.id_reservation = NEW.id_reservation);
+            
     END IF
 END
