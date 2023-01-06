dc8ce89455f3aae49ed85a893e20c0816c8fc16a
vdang <vdang@enssat.fr>
2023-01-06 13:47:56 +0100

    Procédure de calcul du tarif actuel d'un circuit

--- automatisation/calculTarifActuel.sql
+++ automatisation/calculTarifActuel.sql
@@ -1,10 +1,17 @@
 CREATE PROCEDURE `calculTarifActuel`(IN IDperiode INT, IN IDcircuit INT, OUT tarifActuel DECIMAL(8,2))
 BEGIN
-	SELECT tarif
-    FROM circuit
 
-    WHERE id_circuit = IDcircuit AND
+    /*
+    On récupère le circuit à partir de sa clé primaire
+    On récupère le tarif actuel du circuit par son attribut associé
+    */
+
+	SELECT circuit.tarif
+    FROM circuit
+    WHERE
+    id_circuit = IDcircuit AND
     id_periode = IDperiode
 
     INTO tarifActuel;
+    
 END
